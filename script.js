27ec6e1acba50bd36c073d67fc55390cdb102935
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-03-20 15:23:11 +0700

    Create validation on form. check if input is empty and valid

--- script.js
+++ script.js
@@ -30,10 +30,37 @@ const modal = document.querySelectorAll(".modalDescription");
 const buttonClose = document.querySelectorAll(".close");
 
 for (let i = 0; i < btn.length; i++) {
-    btn[i].addEventListener('click', ()=>{
-        modal[i].style.display = "block";
-    })
-    buttonClose[i].addEventListener('click', ()=> {
-        modal[i].style.display = "none";
-    })
+  btn[i].addEventListener("click", () => {
+    modal[i].style.display = "block";
+  });
+  buttonClose[i].addEventListener("click", () => {
+    modal[i].style.display = "none";
+  });
+}
+
+// validation form
+function validation() {
+  const email = document.myForm.email.value;
+  const textarea = document.myForm.message.value;
+  var reg = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  var result = reg.test(email);
+
+  if (email == "" && textarea == "") {
+    alert("Email dan pesan harus diisi!");
+    return false;
+  } else if (email == "") {
+    alert("Email harus diisi!");
+    return false;
+  } else if (textarea == "") {
+    alert("Pesan harus diisi!");
+    return false;
+  } else {
+    if (result == false) {
+      alert("Email yang anda masukkan tidak valid");
+      return false;
+    } else {
+      alert("Terimakasih, pesan anda akan segera kami jawab");
+      return true;
+    }
+  }
 }
